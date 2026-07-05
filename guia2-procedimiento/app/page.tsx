a1799f81fc7d3d34016c8f7fa3b529d676497e0a
Keneth Hernandez <knthnolasco@gmail.com>
2026-07-04 21:32:03 -0600

    Tarea 1

--- guia2-procedimiento/app/page.tsx
+++ guia2-procedimiento/app/page.tsx
@@ -1,69 +1,34 @@
+'use client'; 
+import { useState } from "react";
 import styles from "./page.module.css"
-
-
-interface Jugador {
-  id: number; 
-  nombre: string; 
-  altura: string; 
-  peso: string; 
-}
-
-interface  Equipo { 
-  id: number ; 
-  nombre: string; 
-  plantilla: Jugador[]; 
-}
-
-interface EquiposProps { 
-  equipos : Equipo[]; 
-}
-
-const equiposData: Equipo[] = [
-  { id: 1, nombre: "Real Madrid", plantilla: [
-    { id: 1, nombre: "Vinicius Jr.", altura: "1.76", peso: "73Kg" },
-    { id: 2, nombre: "Jude Bellingham", altura: "1.86", peso: "75Kg" },
-    { id: 3, nombre: "Kylian Mbappé", altura: "1.78", peso: "73Kg" },
-  ]},
-  { id: 2, nombre: "Barcelona", plantilla: [
-    { id: 1, nombre: "Lamine Yamal", altura: "1.80", peso: "67Kg" },
-    { id: 2, nombre: "Robert Lewandowski", altura: "1.85", peso: "81Kg" },
-    { id: 3, nombre: "Gavi", altura: "1.73", peso: "68Kg" },
-  ]},
-];
-
-
-  const Equipos =  ({equipos} : EquiposProps) => (
-
-  <div className="{styles.container_list}"> 
-    <h2>Equipos de Futbol</h2>
-    {equipos.map((equipo: Equipo)=> {
-       return ( <div key={equipo.id}>
-          <h2>{equipo.nombre}</h2>
-          <ul>
-            {equipo.plantilla.map((j : Jugador)=> {
-
-              return (
-              <li key={j.id}>
-                <strong>{j.nombre}</strong> - {j.altura}m . {j.peso}lb 
-              </li>)
-            })}
-          </ul>
-        </div>)
-
-    })}
-
-  </div>
-)
- 
-
 
 
 
 export default function Home() {
+  const [contador, setContador] = useState<number>(0);
+  const [mensaje, setMensaje] = useState<string>("");
+  const incrementar = () => {
+    setMensaje("");
+    setContador(contador + 1);
+  };
+  const decrementar = () => {
+    if (contador > 0) {
+      setMensaje("");
+      setContador(contador - 1);
+    }else {
+      setMensaje("El contador no puede ser menor a 0");
+    }
+  };
+  
   return (
     <main className={styles.main}>
-      <h1>Mi Aplicación de Fútbol</h1>
-      <Equipos equipos={equiposData} />
+      <h1>Contador</h1>
+      <p className={styles.contador}> {contador}</p>
+    <p className={styles.mensaje}> {mensaje}</p>
+      <div className={styles.boton}>
+      <button onClick={incrementar}>Incrementar</button>
+      <button onClick={decrementar}>Decrementar</button>
+      </div>
     </main>
   );
 }
