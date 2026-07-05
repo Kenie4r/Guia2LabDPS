50edf92b357f76b579993bede08592f2888f95e6
Keneth Hernandez <knthnolasco@gmail.com>
2026-07-04 20:31:49 -0600

    Imagenes de jugadores

--- guia2-procedimiento/app/page.tsx
+++ guia2-procedimiento/app/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+import { useState } from "react";
 import styles from "./page.module.css"
+
 
 
 interface Jugador {
@@ -27,24 +30,44 @@ const equiposData: Equipo[] = [
   { id: 2, nombre: "Barcelona", plantilla: [
     { id: 1, nombre: "Lamine Yamal", altura: "1.80", peso: "67Kg" },
     { id: 2, nombre: "Robert Lewandowski", altura: "1.85", peso: "81Kg" },
-    { id: 3, nombre: "Gavi", altura: "1.73", peso: "68Kg" },
+    { id: 3, nombre: "Cubarsi", altura: "1.73", peso: "68Kg" },
   ]},
 ];
 
 
   const Equipos =  ({equipos} : EquiposProps) => (
-
-  <div className="{styles.container_list}"> 
+  
+  <div className={styles.container__list}> 
     <h2>Equipos de Futbol</h2>
     {equipos.map((equipo: Equipo)=> {
        return ( <div key={equipo.id}>
           <h2>{equipo.nombre}</h2>
           <ul>
             {equipo.plantilla.map((j : Jugador)=> {
+              const [imgJugador, setImgJugador] = useState<string | null>(null);
+              let data = null;
+              //vamos a hacer un fetch para poder buscar la api de THESPORTS 
+             // console.log(`https://www.thesportsdb.com/api/v1/json/3/searchplayers.php?p=${encodeURIComponent(j.nombre)}`); 
+              fetch(`https://www.thesportsdb.com/api/v1/json/3/searchplayers.php?p=${encodeURIComponent(j.nombre)}`)
 
+              .then(response => {
+                 data = response.json();
+
+
+                 data.then((data) => { 
+                  setImgJugador(data?.player?.[0]?.strCutout || null);
+                  console.log(imgJugador);
+                 })
+                console.log(data);
+               
+                }).then(() => {
+                   //setImgJugador(data?.player?.[0]?.strCutout || null);
+                  console.log(imgJugador);
+                });
               return (
               <li key={j.id}>
                 <strong>{j.nombre}</strong> - {j.altura}m . {j.peso}lb 
+                <img src={imgJugador || undefined} alt={j.nombre} />
               </li>)
             })}
           </ul>
@@ -60,6 +83,9 @@ const equiposData: Equipo[] = [
 
 
 export default function Home() {
+  
+   // const {imgJugador, setImgJugador} = useState<string | null>(null);
+
   return (
     <main className={styles.main}>
       <h1>Mi Aplicación de Fútbol</h1>
