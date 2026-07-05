d51ee5583c8d76b5b0001b106d3388c828bdb1fe
Keneth Hernandez <knthnolasco@gmail.com>
2026-07-04 21:58:15 -0600

    login tarea3

--- guia2-procedimiento/app/page.tsx
+++ guia2-procedimiento/app/page.tsx
@@ -1,151 +1,57 @@
 'use client'; 
-import { useState } from "react";
-import styles from "./page.module.css"
+import {useState} from 'react';
+
+import style from './page.module.css';
+
+
+interface User {
+  username: string;
+  password: string;
+}
 
 
 
-
-type CoversionType = "CtoF" | "FtoC";
-
-
-
-
-
+const users: User[] = [
+  { username: 'admin', password: 'admin' },
+  { username: 'profesor', password: 'profesor' },
+  { username: 'estudiante', password: 'estudiante' }
+]; 
 
 export default function Home() {
-  const [TempInicial, setTempInicial] = useState<number>(0);
-  const [TempFinal, setTempFinal] = useState<number>(0);
-  const [ConversionType, setConversionType] = useState<CoversionType>("CtoF");
-  const conversion: Record<CoversionType, (temp: number) => number> = {
-    CtoF: (temp) => (temp * 9/5) + 32,
-    FtoC: (temp) => (temp - 32) * 5/9
-  };
+  
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = () => {
+
+    if (username === '' || password === '') {
+      setError('Por favor, complete todos los campos.');
+      return;
+    }
+
+    if (users.some(user => user.username === username && user.password === password)) {
+      setError('');
+      alert('Inicio de sesión exitoso');
+    } else {  
+      setError('Usuario o contraseña incorrectos.');
+     }
+
+  }
+
 
   return (
-    <main className={styles.main}>
-      <h1>Conversor de Temperatura</h1>
-      <div className={styles.form}>
-       
-       <div> <label htmlFor="tempInicial">Temperatura Inicial:</label>
-        <input
-          type="number"
-          id="tempInicial"
-          value={TempInicial}
-          onChange={(e) => setTempInicial(parseFloat(e.target.value))}
-        />
-        </div>
-        <div>
-        <select
-          value={ConversionType}
-          onChange={(e) => setConversionType(e.target.value as CoversionType)}
-        >
-          <option value="CtoF">Celsius a Fahrenheit</option>
-          <option value="FtoC">Fahrenheit a Celsius</option>
-        </select>
-
-        </div>
-        <div>
-        <button onClick={() => setTempFinal(conversion[ConversionType](TempInicial))}>
-          Convertir
-        </button>
-        </div>
-      </div>
-      <div className={styles.result}>
-        <p>Temperatura Final: {TempFinal.toFixed(2)}</p>
-      </div>
+    <main className={style.main}>
+      <h1>Guía 2: Login</h1>
+    <div className={style.form}>
+      <input
+        type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Usuario" />
+      <input
+        type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Contraseña" />
+      <button onClick={handleLogin}>Iniciar sesión</button>
+      {error && <p className={style.error}>{error}</p>}
+    </div>
+    
     </main>
   );
-}
-
-
-/*import Image from "next/image";
-/*import { JSX } from "react/jsx-runtime";
-
-const element: JSX.Element = (
-  <>
-    <h1>Hola, Mundo!</h1>
-    <h2>Son las {new Date().toTimeString()}</h2>
-  </>
-);
-
-export default function Home(){
-
-  return (
-      <main className="{styles.main}">
-          <div className="App">
-            {element}
-          </div>
-
-      </main>
-
-  );
-}
-
-
-
-
-/*
-export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
-}
-*/
+ } 
